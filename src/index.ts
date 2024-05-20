46b2a768eafd32cda8e3720ea9b74e966957a868
tatsuru koike <t-koike@marsflag.com>
2024-05-20 12:44:38 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import fastify from "fastify";
+import multiPart from "@fastify/multipart";
+import cors from "@fastify/cors";
+
+const server = fastify();
+server.register(cors, {
+  origin: "*",
+});
+server.register(multiPart, {
+  attachFieldsToBody: "keyValues",
+  limits: {
+    fileSize: 2147483648, // ファイルサイズの制限 (50MB)
+    fieldSize: 2147483648, // フィールドサイズの制限 (50MB)
+  },
+});
+
+/*******************************************************
+ * CREATE SERVER
+ *******************************************************/
+server.listen({ port: 8080 }, (err, address) => {
+  if (err) {
+    console.error(err);
+    process.exit(1);
+  }
+  console.log(`Server listening at ${address}`);
+});
+
+// API
+server.get("/", async (request, reply) => {
+  return "application\n";
+});
